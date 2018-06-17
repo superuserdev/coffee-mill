2b4019547832536511e5fb6256fb880a891a8341
Chris Zuber <shgysk8zer0@gmail.com>
2018-06-17 11:20:30 -0700

    Add basic functionality & improve button styles

--- js/index.js
+++ js/index.js
@@ -1,5 +1,46 @@
-import {ready, $} from './std-js/functions.js';
+import './std-js/shims.js';
+import './std-js/deprefixer.js';
+import {loaded, $, registerServiceWorker} from './std-js/functions.js';
+import webShareApi from './std-js/webShareApi.js';
+import {
+	facebook,
+	twitter,
+	googlePlus,
+	linkedIn,
+	reddit,
+	gmail,
+} from './std-js/share-config.js';
 
-ready().then(async () => {
-	$(document.documentElement).replaceClass('no-js', 'js');
+webShareApi(facebook, twitter, googlePlus, linkedIn, reddit, gmail);
+
+loaded().then(async () => {
+	if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
+		registerServiceWorker(document.documentElement.dataset.serviceWorker);
+	}
+
+	$('[data-show-modal]').click(event => {
+		const target = event.target.closest('[data-show-modal]');
+		const dialog = document.querySelector(target.dataset.showModal);
+		if (dialog instanceof HTMLElement) {
+			dialog.showModal();
+		}
+	});
+
+	$('[data-close]').click(event => {
+		const target = event.target.closest('[data-close]');
+		const dialog = document.querySelector(target.dataset.close);
+		if (dialog instanceof HTMLElement) {
+			dialog.close();
+		}
+	});
+
+	$('[data-share]').click(event => {
+		event.preventDefault();
+		event.stopPropagation();
+		navigator.share({
+			title: document.title,
+			url: location.href,
+			text: document.querySelector('meta[name="description"][content]').getAttribute('content'),
+		});
+	});
 });
